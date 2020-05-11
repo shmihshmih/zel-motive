39ce7da8299c020bfa1fff6a73961c19c2e89918
anton <anton@localhost.localdomain>
2020-05-11 22:21:31 +0300

    init with vue-cli

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # zel-motive
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+Description
+Зеленодольские мотивы в фольклоре, 
+литературе, 
+музыке, 
+театре, 
+кино и 
+изобразительном искусстве.
+
+Разделы- 
+фольклор, 
+поэзия и проза (подразделы проза, поэзия), 
+музыка, 
+театр, 
+киноискусство, 
+изобразительное искусство (внутри подразделы иконопись, крмпьютерная графика,графика,  скульптура, декоративно прикладное искусство, художественная фотография)
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <ul>
+    <li>
+      <h2>фольклор</h2>
+    </li>
+    <li>
+      <h2>поэзия и проза</h2>
+      <h3>проза</h3>
+      <h3>поэзия</h3>
+    </li>
+    <li>
+      <h2>музыка</h2>
+    </li>
+    <li>
+      <h2>театр</h2>
+    </li>
+    <li>
+      <h2>киноискусство</h2>
+    </li>
+    <li>
+      <h2>изобразительное искусство</h2>
+      <h3>иконопись</h3>
+      <h3>крмпьютерная графика</h3>
+      <h3>графика</h3>
+      <h3>скульптура</h3>
+      <h3>декоративно прикладное искусство</h3>
+      <h3>художественная фотография</h3>
+    </li>
+  </ul>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
